b7f788f8165cc23059d4f4ef42c9c1db91321b96
islambekurazbaev1 <urazbaevislambek1@gmail.com>
2024-01-22 14:43:11 +0500

    sd

---
+++ islambek/includes/post.php
@@ -0,0 +1,18 @@
+<?php
+$title = "Post";
+require 'includes/navbar.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $title; ?></title>
+</head>
+<body>
+    
+</body>
+</html>
+
+<?php require 'includes/footer.php'; ?>
